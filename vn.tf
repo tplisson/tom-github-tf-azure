a1286720bb688f52c7815010bf408607be4520f8
Thomas Plisson <tom@upwindconsulting.net>
2022-03-30 10:03:57 -0400

    Update vn.tf

--- vn.tf
+++ vn.tf
@@ -20,7 +20,7 @@ resource "azurerm_virtual_network" "example" {
     name           = "subnet1"
     address_prefix = "10.0.1.0/24"
   }
-
+  
   subnet {
     name           = "subnet2"
     address_prefix = "10.0.2.0/24"
@@ -30,4 +30,16 @@ resource "azurerm_virtual_network" "example" {
   tags = {
     environment = "Production"
   }
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
 }
