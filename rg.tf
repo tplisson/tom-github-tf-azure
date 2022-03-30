5d489161d3221f44f8211ad2d97c29c80ac22063
Thomas Plisson <tom@upwindconsulting.net>
2022-03-30 15:20:14 -0400

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example5"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example5"
+  location = var.location
+}
