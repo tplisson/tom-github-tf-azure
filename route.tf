a0d1ad42854a23edf52cebfde55bb7517ad3e2cd
Thomas Plisson <tom@upwindconsulting.net>
2022-03-30 15:33:29 -0400

    Update route.tf

--- route.tf
+++ route.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_route_table" "example" {
-  name                = "acceptanceTestRouteTable1"
+  name                = "acceptanceTestRouteTable11"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
