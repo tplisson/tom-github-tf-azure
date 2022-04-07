4273b2722ff7aa2285bbc7b08b538da0e112c3fd
Thomas Plisson <tom@saddlebacksolutions.com>
2022-04-07 19:14:12 -0400

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -47,14 +47,14 @@ resource "azurerm_subnet" "bad" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# resource "azurerm_subnet" "notsobad" {
-#   name                 = "frontend"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vn.name
-#   address_prefixes     = ["10.0.3.0/24"]
-# }
+resource "azurerm_subnet" "notsobad" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
 
-# resource "azurerm_subnet_network_security_group_association" "nsgb" {
-#   subnet_id                 = azurerm_subnet.notsobad.id
-#   network_security_group_id = azurerm_network_security_group.nsg.id
-# }
+resource "azurerm_subnet_network_security_group_association" "nsgb" {
+  subnet_id                 = azurerm_subnet.notsobad.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
