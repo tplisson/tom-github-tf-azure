dc19a91ef8fe51b44ef132b826d97846946fa2f7
Thomas Plisson <tom@saddlebacksolutions.com>
2022-05-06 12:10:47 -0400

    Create vn2.tf

---
+++ vn2.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "example-resources"
+  location = "Canada Central"
+}
+
+resource "azurerm_virtual_network" "fail" {
+  name                = "fail"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
