8c15dde4b7e83c9880c15fc143002d0ac009f116
Tom Plisson <tplisson@paloaltonetworks.com>
2022-09-01 19:49:25 +0200

    2022-09-01

--- nsg.tf
+++ nsg.tf
@@ -5,23 +5,17 @@ resource "azurerm_virtual_network" "vn" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "good1" {
-  name                 = "good subnet1"
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-<<<<<<< HEAD
 resource "azurerm_network_security_group" "nsg1" {
-  name                = "example-nsg"
-=======
-resource "azurerm_network_security_group" "good" {
-  name                = "good nsg"
->>>>>>> 4f2dfdffcd6384a68b98bb85f98c739696f71caf
+  name                = "terrible-nsg1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
   security_rule {
     name                       = "rule100"
     priority                   = 100
@@ -35,32 +29,7 @@ resource "azurerm_network_security_group" "good" {
   }
 }
 
-<<<<<<< HEAD
-resource "azurerm_subnet_network_security_group_association" "assoc1" {
-  subnet_id                 = azurerm_subnet.good1.id
+resource "azurerm_subnet_network_security_group_association" "association1" {
+  subnet_id                 = azurerm_subnet.subnet1.id
   network_security_group_id = azurerm_network_security_group.nsg1.id
-=======
-resource "azurerm_subnet_network_security_group_association" "assoc" {
-  subnet_id                 = azurerm_subnet.good.id
-  network_security_group_id = azurerm_network_security_group.good.id
->>>>>>> 4f2dfdffcd6384a68b98bb85f98c739696f71caf
 }
-
-resource "azurerm_subnet" "bad" {
-  name                 = "bad subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vn.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-# resource "azurerm_subnet" "notsobad" {
-#   name                 = "frontend"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vn.name
-#   address_prefixes     = ["10.0.3.0/24"]
-# }  
-
-# resource "azurerm_subnet_network_security_group_association" "assoc2" {
-#   subnet_id                 = azurerm_subnet.notsobad.id
-#   network_security_group_id = azurerm_network_security_group.nsg.id
-# }
--- vn2.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "fail" {
-  name                = "fail"
-  address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
