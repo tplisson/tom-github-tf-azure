68e6cd73b1b5b00392ad382b4b9bbd5987c89892
Thomas Plisson <tom@upwindconsulting.net>
2022-11-07 12:02:31 -0500

    2022-11-07

--- dynamic/ckv.sh
+++ dynamic/ckv.sh
@@ -1,3 +1,4 @@
-cmd='checkov -f dynamic.tf --external-checks-dir . -c dynamic1 --framework terraform --compact'
+cmd='checkov -d . --external-checks-dir . -c dynamic1 --framework terraform --compact'
+# cmd='checkov -f dynamic.tf --external-checks-dir . -c dynamic1 --framework terraform --compact'
 echo "### Executing: $cmd"
 $cmd
